465f5c4b52593f83d026fb5fdbde8b862db09936
Ottsel <nickhunter808@gmail.com>
2021-01-31 12:41:13 -0800

    Merge issues fixed

--- src/main/java/net/nickhunter/mc/ottselturrets/client/models/tile/LaserTurretTileModel.java
+++ src/main/java/net/nickhunter/mc/ottselturrets/client/models/tile/LaserTurretTileModel.java
@@ -10,11 +10,8 @@ public class LaserTurretTileModel extends TurretTileModel {
     public static final String TEXTURE_LOCATION = "textures/blocks/turret_horizontal.png";
     public static final String ANIMATION_LOCATION = "animations/block/turret.animation.json";
 
-    public static final String YAW_BONE = "head";
-    public static final String PITCH_BONE = "head";
-
     public LaserTurretTileModel() {
-        super(MODEL_LOCATION, TEXTURE_LOCATION, ANIMATION_LOCATION, YAW_BONE, PITCH_BONE);
+        super(MODEL_LOCATION, TEXTURE_LOCATION, ANIMATION_LOCATION);
     }
 
     @Override
