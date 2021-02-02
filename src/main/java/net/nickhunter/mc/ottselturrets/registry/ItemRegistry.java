a553c29dc7e65c6379b4e89a72cd057dee7b6c2b
Ottsel <nickhunter808@gmail.com>
2021-02-02 12:31:29 -0800

    Laser weapon item

--- src/main/java/net/nickhunter/mc/ottselturrets/registry/ItemRegistry.java
+++ src/main/java/net/nickhunter/mc/ottselturrets/registry/ItemRegistry.java
@@ -1,27 +1,32 @@
 package net.nickhunter.mc.ottselturrets.registry;
 
-import net.nickhunter.mc.ottselturrets.OttselTurrets;
-import net.nickhunter.mc.ottselturrets.blocks.BallistaTurretBlock;
-import net.nickhunter.mc.ottselturrets.blocks.LaserTurretBlock;
-import net.nickhunter.mc.ottselturrets.items.TurretBlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.nickhunter.mc.ottselturrets.OttselTurrets;
+import net.nickhunter.mc.ottselturrets.blocks.BallistaTurretBlock;
+import net.nickhunter.mc.ottselturrets.blocks.LaserTurretBlock;
+import net.nickhunter.mc.ottselturrets.client.renderers.item.LaserWeaponItemRenderer;
+import net.nickhunter.mc.ottselturrets.items.LaserWeaponItem;
+import net.nickhunter.mc.ottselturrets.items.TurretBlockItem;
 
 public class ItemRegistry {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
-            OttselTurrets.MOD_ID);
+        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+                        OttselTurrets.MOD_ID);
 
-    public static void init() {
-        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-    }
+        public static void init() {
+                ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        }
 
-    // BlockItems
-    public static final RegistryObject<Item> LASER_TURRET = ITEMS.register(LaserTurretBlock.RESOURCE_NAME,
-            () -> new TurretBlockItem(BlockRegistry.LASER_TURRET.get()));
-    public static final RegistryObject<Item> BALLISTA_TURRET = ITEMS.register(BallistaTurretBlock.RESOURCE_NAME,
-            () -> new TurretBlockItem(BlockRegistry.BALLISTA_TURRET.get()));
+        // BlockItems
+        public static final RegistryObject<Item> LASER_TURRET = ITEMS.register(LaserTurretBlock.RESOURCE_NAME,
+                        () -> new TurretBlockItem(BlockRegistry.LASER_TURRET.get()));
+        public static final RegistryObject<Item> BALLISTA_TURRET = ITEMS.register(BallistaTurretBlock.RESOURCE_NAME,
+                        () -> new TurretBlockItem(BlockRegistry.BALLISTA_TURRET.get()));
 
+        // Items
+        public static final RegistryObject<Item> LASER_WEAPON = ITEMS.register(LaserWeaponItem.RESOURCE_NAME,
+                        () -> new LaserWeaponItem(new Item.Properties().group(ItemGroupRegistry.MAIN).setISTER(() -> LaserWeaponItemRenderer::new)));
 }
